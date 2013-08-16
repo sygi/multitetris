570cef4caa3a47c158908f63f87b30f22ff3943b
Jakub Sygnowski <sygnowski@gmail.com>
2013-08-16 20:20:07 +0200

    compilation errors

--- tests/test_board.py
+++ tests/test_board.py
@@ -3,10 +3,12 @@ import unittest
 class TestBox(unittest.TestCase):
 	def test_create(self):
 		from multitetris.backend.state import Box
-		box = Box()
+		box = Box(0,0,(1,2,3))
 
 class TestBoard(unittest.TestCase):
-	pass
+	def test_create_board(self):
+		from multitetris.backend.board import Board
+		board = Board(100, 100)
 
 unittest.main()
 
