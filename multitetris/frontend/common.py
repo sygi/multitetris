7b1b9a6782f36022a2334e4cf3e5ac1e5289ae4a
Patryk Hes <pathes@github.com>
2013-08-16 15:21:44 +0200

    Created common.py in frontend.

---
+++ multitetris/frontend/common.py
@@ -0,0 +1,21 @@
+"""
+Usage:
+
+from common import consts
+-or-
+from common import consts as frontend_consts
+
+and so on.
+"""
+
+consts = {
+    'block_element_size': 5,
+    'columns_per_player': 10,
+    'max_players': 10,
+    'window_width': 800,
+    'window_height': 600,
+}
+
+config = {
+    'max_fps': 60,
+}
--- multitetris/frontend/running_game.py
+++ multitetris/frontend/running_game.py
@@ -1,15 +1,12 @@
-
 import pygame
-from pygame.locals import * # constants
+from pygame.locals import *  # constants
+from common import consts
 
 pygame.init()
-fpsClock = pygame.time.Clock() # FPS
+fpsClock = pygame.time.Clock()  # FPS
 
-const_maxwindowwidth = 20 * 10 * 5 # maxplayers * columnsperplayer * blockelementsize
-const_maxwindowgeight = 50 * 5 + 300 # rowsperplayer * blockelementsize + whatever
-
-windowSurfaceObj = pygame.display.set_mode((const_maxwindowwidth, const_maxwindowgeight))
-pygame.display.set_caption("The game of Multitetris is running.") # window title
+windowSurfaceObj = pygame.display.set_mode((consts.window_width, consts.window_height))
+pygame.display.set_caption("The game of Multitetris is running.")  # window title
 
 fontObj = pygame.font.Font('freesansbold.ttf', 30)
 
@@ -17,6 +14,6 @@ semirandomColor = pygame.Color(123, 230, 58)
 
 while True:
     windowSurfaceObj.fill(semirandomColor)
-	
-    pygame.display.update() # speak your mind!
+
+    pygame.display.update()  # speak your mind!
     fpsClock.tick(60)
