ce53c642abf372e35e3aeca09b6692f0fbe73547
Michal Kowalczyk <mkow13@gmail.com>
2013-08-16 15:56:27 +0200

    Working window + controls

--- multitetris/frontend/running_game.py
+++ multitetris/frontend/running_game.py
@@ -1,19 +1,37 @@
 import pygame
 from pygame.locals import *  # constants
-from common import consts
+from common import *
 
+########################
+# PyGame init
+########################
 pygame.init()
-fpsClock = pygame.time.Clock()  # FPS
+fps_clock = pygame.time.Clock()  # FPS limiter
 
-windowSurfaceObj = pygame.display.set_mode((consts.window_width, consts.window_height))
-pygame.display.set_caption("Multitetris")  # window title
+display = pygame.display.set_mode((consts['window_width'], consts['window_height']))
+pygame.display.set_caption("Multitetris")
 
 fontObj = pygame.font.Font('freesansbold.ttf', 30)
+quit_request = False
 
-semirandomColor = pygame.Color(123, 230, 58)
+# Controls
+mousex,mousey = 0,0
 
-while True:
-    windowSurfaceObj.fill(semirandomColor)
+########################
+# Main loop
+########################
+while not quit_request:
+    display.fill(pygame.Color(0, 0, 0))
 
-    pygame.display.update()  # speak your mind!
-    fpsClock.tick(60)
+    for event in pygame.event.get():
+    	if   event.type == QUIT:
+    		quit_request = True
+    	elif event.type == MOUSEMOTION:
+    		mousex, mousey = event.pos
+    	elif event.type == KEYDOWN:
+    		if event.key == K_ESCAPE:
+    			quit_request = True
+    pygame.display.update()
+    fps_clock.tick(60)
+
+pygame.quit()
