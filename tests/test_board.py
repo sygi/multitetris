59772845a7eadece0bc7080fa5e969ff9a0972ec
Jakub Sygnowski <sygnowski@gmail.com>
2013-08-16 17:07:19 +0200

    added one simple test

---
+++ tests/test_board.py
@@ -0,0 +1,12 @@
+import unittest
+
+class TestBox(unittest.TestCase):
+	def test_create(self):
+		from multitetris.backend.state import Box
+		box = Box()
+
+class TestBoard(unittest.TestCase):
+	pass
+
+unittest.main()
+
