a62b76789caf3b815079f7840217d03276ce9d2e
Mateusz Zieliński <mz11@gimakad.torun.pl>
2013-08-17 16:11:43 +0200

    Added game screen

--- multitetris/frontend/common.py
+++ multitetris/frontend/common.py
@@ -14,8 +14,8 @@ class consts:
     pass
 
 consts.__dict__ = {
-    'block_element_size': 5,
-    'number_of_rows': 30,
+    'block_element_size': 12,
+    'number_of_rows': 46,
     'columns_per_player': 10,
     'max_players': 10,
     'window_width': 800,
@@ -26,4 +26,7 @@ colors = {
     'semirandom': Color(123, 230, 58),
     'block_inner': Color(192, 73, 239),
     'block_border': Color(23, 45, 32),
+    'line': Color(230, 230, 230),
+    'back': Color(10, 60, 160),
+    'text': Color(140, 200, 30),
 }
--- multitetris/frontend/running_game.py
+++ multitetris/frontend/running_game.py
@@ -3,36 +3,72 @@ from pygame.locals import *
 import socket
 import json
 
-from .common import consts
+from .common import consts, colors
 
-bricks = []
+SIZE = (consts.block_element_size, consts.block_element_size)
 
-def draw_game(display):
-    line_color = (100, 100, 100)
-    blockpixsize = consts['block_element_size']
+board_topleft = (22, 32)
+playersline_topleft = (20, 5)
+playersline_height = 20
 
-    state = read_state()
+pygame.init()
+#fontObj = pygame.font.Font("freesansbold.ttf", 10)
+fontObj = pygame.font.SysFont("arial", 17)
 
-    display.fill((123, 230, 58))
-
-SIZE = (15, 15)
+no_of_players = 7
+players_names = ["Player 1", "Player 2", "Player 3", "Player 4",
+            "Player 5", "Player 6", "Player 7", "Player 8"]
 
 def screen_pos(pos):
-    return (pos[0] * SIZE[0],
-            consts.window_height - pos[1] * SIZE[1])
+    return (board_topleft[0] + pos[0] * SIZE[0],
+            board_topleft[1] + (consts.number_of_rows - pos[1] - 1) * SIZE[1])
 
 def read_state():
     return json.loads(sockf.readline())
 
+def draw_box(display, color, screenpos):
+    pygame.draw.rect(display, color,
+                 (screenpos[0] + 1, screenpos[1] + 1) + (SIZE[0] - 1, SIZE[1] - 1), 0)
+
+def draw_main_grid(display):
+    # board
+    pygame.draw.rect(display, colors['line'],
+                (board_topleft[0] - 2,
+                 board_topleft[1] - 2,
+                 consts.columns_per_player * SIZE[0] * no_of_players + 4,
+                 consts.number_of_rows * SIZE[1] + 4
+                ), 2)
+    # players_line
+    pygame.draw.rect(display, colors['line'],
+                (playersline_topleft[0],
+                 playersline_topleft[1],
+                 consts.columns_per_player * SIZE[0] * no_of_players + 4,
+                 25
+                ), 2)
+    for i in range(no_of_players):
+        pygame.draw.rect(display, colors['line'],
+                (playersline_topleft[0] + 1 + consts.columns_per_player * SIZE[0] * i,
+                 playersline_topleft[1] + 2,
+                 2,
+                 25
+                ), 1)
+        text_surface = fontObj.render(players_names[i], False, colors['text'])
+        text_rect = text_surface.get_rect()
+        text_rect.topleft = (playersline_topleft[0] + 5 + consts.columns_per_player * SIZE[0] * i + 10,
+                         playersline_topleft[1] + 3)
+        display.blit(text_surface, text_rect)
+        
+
 def draw_game(display):
+    draw_main_grid(display)
     state = read_state()
     print state
 
     for brick in state['bricks']:
         pos = screen_pos(brick['pos'])
         print pos + SIZE, brick['color']
-        pygame.draw.rect(display, brick['color'],
-                         pos + SIZE, 0)
+        draw_box(display, brick['color'], pos + SIZE)
+
 
 def run():
     global sock, sockf
@@ -40,7 +76,6 @@ def run():
     sock.connect(('localhost', 9999))
     sockf = sock.makefile('r+')
 
-    pygame.init()
     fps_clock = pygame.time.Clock()  # FPS limiter
 
     display = pygame.display.set_mode((
