9df5070ed42b466f7a189aa639be36ce1d823d6c
Michał Zieliński <michal@zielinscy.org.pl>
2013-08-17 15:15:46 +0200

    commit

--- multitetris/backend/game.py
+++ multitetris/backend/game.py
@@ -79,17 +79,17 @@ class Game(object):
             player_brick.rotate()
             if player_brick.is_collision_with_board(self.board):
                 player_brick.rotate_back();
-        else if ch == 'L'
+        elif ch == 'L':
             player_brick.move_left()
-            if player_brick.is_collision_with_board(self.board)
+            if player_brick.is_collision_with_board(self.board):
                 player_brick.move_right()
-        else if ch == 'R'
+        elif ch == 'R':
             player_brick.move_right()
-            if player_brick.is_collision_with_board(self.board)
+            if player_brick.is_collision_with_board(self.board):
                 player_brick.move_left()
-        else if ch == 'D'
+        elif ch == 'D':
             player_brick.move_down()
-            if player_brick.is_collision_with_board(self.board)
+            if player_brick.is_collision_with_board(self.board):
                 player_brick.move_up()
         print('MOVE %r' % ch)
 
